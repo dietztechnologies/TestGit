4d1d14bfc08d459fa00a985cf7c504efb1d01759
dietztechnologies <dietztechnologies@gmail.com>
2018-03-04 21:28:37 -0500

    mysqli test

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php 
+include 'config/conn.php';
+
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+if ($result = $mysqli->query("SELECT * FROM employees LIMIT 10")) {
+    printf("Select returned %d rows.\n", $result->num_rows);
+
+    /* free result set */
+    $result->close();
+}
 ?>
 <html>
 <link rel="stylesheet" type="text/css" href="styles/text.css">
